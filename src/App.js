90dfd124b413a43635df45f10becc08469fedd62
sanket <sanketsondkar@gmail.com>
2024-02-13 00:08:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <Heart />
+
+  );
+}
+
+function Heart(props) {
+  let name = props.name?props.name:"From Sank8";
+  return (
+    <div class="container">
+      <div class="valentines">
+        <div class="envelope"></div>
+        <div class="front"></div>
+        <div class="card">
+          <div class="text">Happy<br /> Valentine's<br /> Day! <br/>{name}</div>
+        </div>
+      </div>
+      <div class="hearts">
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+        <div class="four"></div>
+        <div class="five"></div>
+      </div>
+      <div class="shadow"></div>
+    </div>
+  )
+}
+
+export default App;
