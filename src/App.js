f1309dec53325f33af27f5c03a091e32727046ce
sanket <sanketsondkar@gmail.com>
2024-02-14 01:50:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const query = new URLSearchParams(window.location.search);
   const token = query.get('token')
-  console.log(token)
+  const [isClicked, setClick] = useState();
+  const setEnvolope = () => {
+    setClick(!isClicked);
+    setTimeoutBit(false);
+  }
+  const [timeoutBit, setTimeoutBit] = useState();
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeoutBit(true);
+    }, 15000);
+  }, [])
   return (
-    <Heart name={token} />
+    <Heart name={token} isClicked={isClicked} setEnvolope={setEnvolope} timeoutBit={timeoutBit} />
   );
 }
 
 function Heart(props) {
   let name = props.name ? props.name : "From Sank8";
   return (
-    <div class="container">
-      <div class="valentines">
-        <div class="envelope"></div>
-        <div class="front"></div>
-        <div class="card">
-          <div class="text">Happy<br /> Valentine's<br /> Day! <br /><span>{name}</span></div>
+    <div className="container" onClick={props.setEnvolope}>
+      {
+        (!props.isClicked && props.timeoutBit) && <span className='t-l'>Do Something... open the envelope</span>
+      }
+
+      <div className={`valentines ${props.isClicked ? "open" : "close"}`}>
+        <div className={`envelope ${props.isClicked ? "glow" : ""}`}></div>
+        <div className="front"></div>
+        <div className="card">
+          <div className="text">Happy<br /> Valentine's<br /> Day! <br /><span>{name}</span></div>
         </div>
       </div>
-      <div class="hearts">
-        <div class="one"></div>
-        <div class="two"></div>
-        <div class="three"></div>
-        <div class="four"></div>
-        <div class="five"></div>
+      <div className="hearts">
+        <div className="one"></div>
+        <div className="two"></div>
+        <div className="three"></div>
+        <div className="four"></div>
+        <div className="five"></div>
       </div>
-      <div class="shadow"></div>
+      <div className="shadow"></div>
     </div>
   )
 }
