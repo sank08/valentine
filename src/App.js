14d67eb771fa199c5a02f749baace3e48cbea3c1
sanket <sanketsondkar@gmail.com>
2024-02-13 20:05:21 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 
 function App() {
+  const query = new URLSearchParams(window.location.search);
+  const token = query.get('token')
+  console.log(token)
   return (
-    <Heart />
+    <Heart name={token} />
   );
 }
 
 function Heart(props) {
-  let name = props.name?props.name:"From Sank8";
+  let name = props.name ? props.name : "From Sank8";
   return (
     <div class="container">
       <div class="valentines">
         <div class="envelope"></div>
         <div class="front"></div>
         <div class="card">
-          <div class="text">Happy<br /> Valentine's<br /> Day! <br/>{name}</div>
+          <div class="text">Happy<br /> Valentine's<br /> Day! <br /><span>{name}</span></div>
         </div>
       </div>
       <div class="hearts">
